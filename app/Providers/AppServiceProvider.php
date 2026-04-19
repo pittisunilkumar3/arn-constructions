3813f24e40e67bf1ddda7d38a0fd0f99615dafb0
sunilaicoe <sunil@aicoe.io>
2026-04-19 17:15:03 +0530

    Updated config files, build assets, and hero image

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Storage;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Force asset/storage URLs to use APP_URL
+        // This fixes images when site is in a subfolder (e.g., XAMPP)
+        if (config('app.url')) {
+            URL::forceRootUrl(config('app.url'));
+        }
+
+        // Override Storage::url() to return full URL with APP_URL
+        Storage::disk('public')->buildTemporaryUrlsUsing(function ($path, $expiration) {
+            return url('/storage/' . $path);
+        });
     }
 }
